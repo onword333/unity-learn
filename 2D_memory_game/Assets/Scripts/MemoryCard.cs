96e7ccdc315d9bbc0e3584371eba267d3abeb014
Yura Klepov <onword333@gmail.com>
2021-01-12 00:16:23 +0300

    Лицевая карта открывается при нажатии на ЛКМ

---
+++ 2D_memory_game/Assets/Scripts/MemoryCard.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryCard : MonoBehaviour
+{
+    [SerializeField] private GameObject cardBack;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnMouseDown()
+    {
+        Debug.Log("Клик по карте");
+
+        if (cardBack.activeSelf)
+        {
+            cardBack.SetActive(false);
+        }
+    }
+}
