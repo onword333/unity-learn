4923696f2ab35fccb24fddd8ec5c92994c9bb33b
Yura Klepov <onword333@gmail.com>
2021-01-12 01:11:20 +0300

    Добавлены остальные карты

--- 2D_memory_game/Assets/Scripts/MemoryCard.cs
+++ 2D_memory_game/Assets/Scripts/MemoryCard.cs
@@ -5,26 +5,26 @@ using UnityEngine;
 public class MemoryCard : MonoBehaviour
 {
     [SerializeField] private GameObject cardBack;
+    [SerializeField] private SceneController controller;
 
-    // Start is called before the first frame update
-    void Start()
+    private int _id;
+    public int id
     {
-        
+        get { return _id; }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetCard(int id, Sprite image) 
     {
-        
+        _id = id;
+        GetComponent<SpriteRenderer>().sprite = image;
     }
 
     public void OnMouseDown()
     {
-        Debug.Log("Клик по карте");
-
         if (cardBack.activeSelf)
         {
             cardBack.SetActive(false);
+            //controller.CardRevealed(this);
         }
     }
 }
---
+++ 2D_memory_game/Assets/Scripts/SceneController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+
+    public const int gridRows = 2;
+    public const int gridCols = 4;
+    public const float offsetX = 2f;
+    public const float offsetY = 2.5f;
+
+
+    [SerializeField] private MemoryCard originCard;
+    [SerializeField] private Sprite[] images;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 startPos = originCard.transform.position;
+
+        for (int i = 0; i < gridCols; i++)
+        {
+            for (int j = 0; j < gridRows; j++)
+            {
+                MemoryCard card;
+                if (i == 0 && j == 0)
+                {
+                    card = originCard;
+                } 
+                else 
+                {
+                    card = Instantiate(originCard) as MemoryCard;
+                }
+
+                int _id = Random.Range(0, images.Length);
+                card.SetCard(_id, images[_id]);
+
+                float posX = (offsetX * i) + startPos.x;
+                float posY = -(offsetY * j) + startPos.y;
+                card.transform.position = new Vector3(posX, posY, startPos.z);
+            }
+        }
+
+
+        int id = Random.Range(0, images.Length);
+        originCard.SetCard(id, images[id]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
