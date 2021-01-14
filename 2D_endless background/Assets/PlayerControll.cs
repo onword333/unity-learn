07d1223a4fd1b0f47af52c16ceaa61285b90fdf2
Yura Klepov <onword333@gmail.com>
2021-01-15 00:08:10 +0300

    Игроку добавлены прыжки и анимация

--- 2D_endless background/Assets/PlayerControll.cs
+++ 2D_endless background/Assets/PlayerControll.cs
@@ -5,21 +5,51 @@ using UnityEngine;
 public class PlayerControll : MonoBehaviour
 {
 
+    private Collider2D myCollider;
+    private Rigidbody2D rb2d;
+
     [SerializeField] private Animator animator;
+    [SerializeField] private bool isGrounded;
+    [SerializeField] private LayerMask whatIsGround;
+    [SerializeField] private float jumpForce;
+
+    private bool isJumping = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        myCollider = GetComponent<Collider2D>();
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Jump"))
+        // для PC
+        isJumping = Input.GetButtonDown("Jump");
+
+        // для mobile
+        if (Input.touchCount > 0)
         {
-            animator.SetBool("isJump", true);
-        }        
-        
+            isJumping = true;
+        }
+
+        if (isJumping && isGrounded)
+        {
+            Vector2 movement = Vector2.up * jumpForce;
+            rb2d.AddForce(movement, ForceMode2D.Impulse);
+            animator.SetBool("isJump", true);   // включаем анимацию прыжка
+            isGrounded = false;
+        }
+    }
+
+    private void FixedUpdate() {}    
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // проверяем, что игрок действительно прикоснулся к земле
+        // т.е. к определенному объекту слой которого ground
+        isGrounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround); 
+        animator.SetBool("isJump", !isGrounded);
     }
 }
