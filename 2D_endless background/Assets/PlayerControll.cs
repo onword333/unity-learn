eef3765d254029ebf2257e8091dc3a6f560edba8
Yura Klepov <onword333@gmail.com>
2021-01-14 20:04:33 +0300

    Добавлен персонаж и анимация бега

---
+++ 2D_endless background/Assets/PlayerControll.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControll : MonoBehaviour
+{
+
+    [SerializeField] private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            animator.SetBool("isJump", true);
+        }        
+        
+    }
+}
