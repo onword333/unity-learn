0d4e01ac077b5096f9e38269bb564bc8240ee383
Yura Klepov <onword333@gmail.com>
2021-01-23 16:25:34 +0300

    Рефакторинг кода

---
+++ 2D_endless background/Assets/Player.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    
+    [SerializeField] private float jumpForce;
+    [SerializeField] private Animator animator;
+
+    private Rigidbody2D rb2d;
+    private bool isJumping;
+    private bool onGround = true;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // для PC
+        isJumping = Input.GetButtonDown("Jump");
+
+        // для mobile
+        if (Input.touchCount > 0)
+        {
+            isJumping = true;
+        }
+    
+        jump();
+    }
+
+    private void jump() 
+    {
+        if (isJumping && onGround)
+        {
+            Vector2 movement = Vector2.up * jumpForce;
+            rb2d.AddForce(movement, ForceMode2D.Impulse);
+            onGround = false;
+            animator.SetBool("isJump", true);
+        }        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        onGround = true;
+        animator.SetBool("isJump", false);
+    }
+}
--- 2D_endless background/Assets/old_PlayerControll.cs
+++ 2D_endless background/Assets/old_PlayerControll.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControll : MonoBehaviour
+public class oldPlayerControll : MonoBehaviour
 {
 
     private Collider2D myCollider;
