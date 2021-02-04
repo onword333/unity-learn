a12ebdf09d75b15690c9c7fc1e7431681f6d7862
Yura Klepov <onword333@gmail.com>
2021-02-04 11:41:39 +0300

    Испрвлена проблема генерации препятствий
    После перемещения сгенерированных препятствий необходимо сбросить
    velocity = Vector2.zero т.к. предметы после передвижения фона падают
    и набирают скорость, после этого создается эфект проскакивания через
    землю.

---
+++ 2D_endless background/Assets/Scripts/EnemyPool.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPool : MonoBehaviour
+{
+    public int enemiesPoolSize = 5;
+    public GameObject enemiesPrefab;
+    public float spanwRate = 4f;
+
+    private Vector2 objectPoolPosition = new Vector2(-15f, -7f);
+    private GameObject[] enemies;
+    private float timeSinceLastSpawned;
+    private float spawnYPosition = -1f;
+    private int currentEnemy = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemies = new GameObject[enemiesPoolSize];
+        for (int i = 0; i < enemiesPoolSize; i++)
+        {
+            enemies[i] = (GameObject) Instantiate(enemiesPrefab, objectPoolPosition, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeSinceLastSpawned += Time.deltaTime;
+
+        if (GameControll.instance.gameOver == false && timeSinceLastSpawned >= spanwRate)
+        {
+            timeSinceLastSpawned = 0;
+            float spawnXPosition = Random.Range(11f, 17f);
+            enemies[currentEnemy].transform.position = new Vector2(spawnXPosition, spawnYPosition);
+            Rigidbody2D rb2d = enemies[currentEnemy].GetComponent<Rigidbody2D>();
+            rb2d.velocity = Vector2.zero;
+            currentEnemy++;
+            if (currentEnemy >= enemiesPoolSize)
+            {
+                currentEnemy = 0;
+            }
+        }
+    }
+}
--- 2D_endless background/Assets/Scripts/Player.cs
+++ 2D_endless background/Assets/Scripts/Player.cs
@@ -34,7 +34,7 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
-        jump();
+        //jump();
     }
 
     private void jump() 
